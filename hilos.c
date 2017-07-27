b5ce5ff50b90bc16fa29c5f5bbbda6c6058a7c32
liarias <lucio_arias96@hotmail.com>
2017-07-27 15:20:12 -0500

    ..

--- hilos.c
+++ hilos.c
@@ -5,11 +5,30 @@
 #include <pthread.h>
 
 int aleatorio(int min,int max);
+double obtenerTiepoActual();
+
+int aleatorio(int min,int max){
+	return(rand()%(max-min+1))+min;
+}
+
+double obtenerTiepoActual(){
+	struct timespec tsp;
+	clock_gettime(CLOCK_REALTIME,&tsp);
+	double secs = ();
+	double nano = ();
+	return secs + nano;
+}
+
 int main(int argc, char *argv[]){
 	if (argc!=3){
-		printf("%s","ingrese correctamente los argumentos");
+		printf("%s\n","ingrese correctamente los argumentos");
 		return 0;
 	}
 	int tamanio = atoi(argv[1]);
-	int nHilos = atoi(argv[2]);	
+	int nHilos = atoi(argv[2]);
+	int *arreglo;
+	arreglo = (int*)malloc(tamanio*sizeof(int));
+	
 }
+
+

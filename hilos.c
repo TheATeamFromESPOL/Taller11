7da5b2318678b33c2f9d6acccf5231081db31cba
liarias <lucio_arias96@hotmail.com>
2017-07-27 15:05:27 -0500

    ..

--- hilos.c
+++ hilos.c
@@ -4,4 +4,12 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-
+int aleatorio(int min,int max);
+int main(int argc, char *argv[]){
+	if (argc!=3){
+		printf("%s","ingrese correctamente los argumentos");
+		return 0;
+	}
+	int tamanio = atoi(argv[1]);
+	int nHilos = atoi(argv[2]);	
+}
